979a8b1138ad5e6f4e741095367ea6f94b4f403b
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-24 17:03:15 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,4 +4,34 @@ class Patient
   
   @@all = []
   
+  def initialize(name)
+    
+    @name = name
+    @@all << self
+    
+  end
+  
+  def self.all
+    
+    @@all
+    
+  end
+  
+  def new_appointment(date, doctor)
+    
+    Apointment.new(date, self, doctor)
+    
+  end
+  
+  def appointments
+    
+    Appointment.all.select{|appointment| appointment.patient == self}
+    
+  end
+  
+  def doctors
+    
+    appointments.collect{|appointment|appointment.doctor}
+    
+  end
 end
